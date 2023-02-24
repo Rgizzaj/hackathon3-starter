13b4a6c46b22c0083be81f4ecc3c6cfd5f240a2e
Leonid Selivanov <rgizzaj@icloud.com>
2023-02-24 15:49:07 +0100

    style

--- resources/views/animals/detail.blade.php
+++ resources/views/animals/detail.blade.php
@@ -1,13 +1,12 @@
-<div>
-    <ul>
-        <li>
-          <div>
-              Name:  {{$detail->name }} <br>
-              Species:  {{$detail->species}}<br>
-              Breed:  {{$detail->breed }}<br>
-              Age:  {{$detail->age }}<br>
-              Weight:  {{$detail->weight }}
-          </div>
-        </li>
-    </ul>
-</div>
+      @include('common.htmlUpper')
+      <div class="pet-details">
+            <h1>Pet Details: </h1>
+                <div class="details">
+                  Name:  {{$detail->name }} <br>
+                  Species:  {{$detail->species}}<br>
+                  Breed:  {{$detail->breed }}<br>
+                  Age:  {{$detail->age }}<br>
+                  Weight:  {{$detail->weight }}
+                </div>
+      </div>
+@include('common.htmlBottom')
--- resources/views/animals/index.blade.php
+++ resources/views/animals/index.blade.php
@@ -1,14 +1,21 @@
-<div>
-    <ul>
-@foreach ($animalList as $animal)
-    <li>
-        {{$animal->owner->first_name}}`s:
+@include('common.htmlUpper')
 
-      <a href="{{route('animals.detail', ['id'=> $animal->id])}}">{{$animal->name}}</a>  
-            <div class="image">
-                <img src="/images/pets/{{ $animal->image->path }}" alt="">
-            </div>
-    </li>
-@endforeach;
-    </ul>
-</div>
+<h1>List of our pawcients: </h1>
+
+
+    @foreach ($animalList as $animal)
+
+        <div class="index">
+
+                <div class="owner">{{$animal->owner->first_name}}`s:    
+                    <a href="{{route('animals.detail', ['id'=> $animal->id])}}">{{$animal->name}}</a>  
+                </div>
+
+                <div class="image">
+                    <img src="/images/pets/{{ $animal->image->path }}" alt="" width="500" height="400">
+                </div>
+        </div>
+
+    @endforeach;
+   
+@include('common.htmlBottom')
